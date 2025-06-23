fdf7c59fdc993e7188bf314fe940da2f0ab998b5
richardamasah <richard.amasah@amalitechtraining.org>
2025-06-23 13:21:46 +0000

    add sql queriesfor top spenders

--- Athena-queries/query.sql
+++ Athena-queries/query.sql
@@ -9,3 +9,15 @@ ORDER BY
   total_revenue DESC
 LIMIT 1;
 
+
+--Identify top-spending users
+SELECT 
+  user_id, 
+  first_name, 
+  last_name, 
+  user_total_spent
+FROM 
+  rental.user_metrics
+ORDER BY 
+  user_total_spent DESC
+LIMIT 5;
